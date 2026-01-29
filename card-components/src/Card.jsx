f4227039a598c0119df2fe0b323ce9c0ed12370e
codevibex <ash292@protonmail.com>
2026-01-29 11:09:31 +0000

    Added card components for rendering Used inline-block for card flexibility across devices

---
+++ card-components/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return(
+       <div className="card">
+        <img  className="card-image"src="https://i.imgur.com/oClXBPVt.jpeg" alt="cat picture"></img>
+        <h2 className="card-title">Cat</h2>
+        <p className="card-text">Cat sitting in the snow</p>
+       </div> 
+    );
+}
+
+export default Card
